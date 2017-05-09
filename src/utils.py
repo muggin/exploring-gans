c5263f6523489b0142c1df16d63f557c2fb7a877
Ubuntu <ubuntu@ip-172-31-38-202.eu-west-1.compute.internal>
2017-05-09 12:20:58 +0000

    Moved notebooks, added utils files

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_images(images, image_dims, max_col=4, file_name='', to_file=False):
+    rows = (len(images)-1)// max_col + 1
+    for ix, image in enumerate(images):
+        plt.subplot(rows, max_col, ix+1)
+        plt.imshow(image.reshape(image_dims), cmap='gray_r', interpolation=None)
+    plt.tight_layout()
+    plt.show()
+
+def plot_performance(*metrics):
+    """
+    function plots various performance metrics over time.
+    provide multiple tuples in the format (metric-name, metric).
+    different metrics (tuples) should be provided as consecutive arguments
+    """
+    plt.figure()
+    for label, metric in metrics:
+        epochs = range(len(metric))
+        plt.plot(metric, label=label)
+    plt.grid()
+    plt.legend()
+    plt.show()
