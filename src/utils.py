ff54a581fa24c4207489e0ec2f7bbdb776409a3f
root <root@ip-172-31-39-109.eu-west-1.compute.internal>
2017-05-11 06:59:00 +0000

    Notebooks and utils as of Thursday morning

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 
-def plot_images(images, image_dims, max_col=4, file_name='', to_file=False):
+from time import strftime
+
+def plot_images(images, image_dims, max_col=4, to_file=False):
     rows = (len(images)-1)// max_col + 1
     for ix, image in enumerate(images):
         plt.subplot(rows, max_col, ix+1)
-        plt.imshow(image.reshape(image_dims), cmap='gray_r', interpolation=None)
+        plt.imshow(image.reshape(image_dims), cmap='gray_r', interpolation='none')
 	plt.xticks([])
 	plt.yticks([])
     plt.tight_layout()
+
+    if to_file:
+        fig_name = 'samples_{}.png'.format(time_now())
+        plt.savefig(fig_name, dpi=120)
     plt.show()
 
 def plot_performance(*metrics):
@@ -24,4 +30,9 @@ def plot_performance(*metrics):
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
+    fig_name = 'performance_{}.png'.format(time_now())
+    plt.savefig(fig_name, dpi=120)
     plt.show()
+
+def time_now():
+    return str(strftime("%Y-%m-%d  %H:%M:%S"))
